7f8fc79b4eee45eb9b66dadffbbb8bbcf4a61ce9
Dea Leon <deadeveras@yahoo.com>
2022-08-19 16:38:59 +0200

    fixed test2 & added nested folders

--- test_tmp_path.py
+++ test_tmp_path.py
@@ -6,7 +6,7 @@ import pprint
 
 DIRECTORY1 = "Dir_de_prueba1/"#, "Dir_de_prueba2/folder1/folder2/folder3")
 
-DIRECTORY2 = "Dir_de_prueba2/folder2/"
+DIRECTORY2 = "Dir_de_prueba2/"
 
 """
 DIRECTORY2 =[('/Users/dealeon/Dir_de_prueba/X661.jpg', '/Users/dealeon/Dir_de_prueba'),
@@ -44,12 +44,12 @@ def create_dir1(tmp_path_factory):
     newtemp_dir = tmp_path_factory.mktemp(DIRECTORY1, numbered=False)
     yield newtemp_dir
     shutil.rmtree(str(newtemp_dir), ignore_errors=False)
-    shutil.rmtree(str(newtemp_dir.parent), ignore_errors=False)
+    #shutil.rmtree(str(newtemp_dir.parent), ignore_errors=False)
 
 
 def test_1(create_dir1):
     file = '/Users/dealeon/Dir_deprueba1/X661.jpg'
-    print(create_dir1)
+    print("test 1", create_dir1)
     print(file)
 
     path_with_file = shutil.copy(file, create_dir1, follow_symlinks=True)
@@ -67,12 +67,16 @@ def test_1(create_dir1):
 @pytest.fixture(scope="session")
 def create_dir2(tmp_path_factory):
 
-    newtemp_dir0 = tmp_path_factory.mktemp( "Dir_de_prueba2/", numbered=False)
+    newtemp_dir0 = tmp_path_factory.mktemp( DIRECTORY2, numbered=False)
+    #newtemp_dir = newtemp_dir0 / "folder1"
+
     newtemp_dir = newtemp_dir0 / "folder1"
     newtemp_dir.mkdir()
+    newtemp_dir2 = newtemp_dir / "folder2"
+    newtemp_dir2.mkdir()
     yield newtemp_dir0
     shutil.rmtree(str(newtemp_dir0), ignore_errors=False)
-    shutil.rmtree(str(newtemp_dir0.parent), ignore_errors=False)
+    #shutil.rmtree(str(newtemp_dir0.parent), ignore_errors=False)
 
 def test_2(create_dir2):
     file = '/Users/dealeon/Dir_deprueba1/X661.jpg'
@@ -81,22 +85,18 @@ def test_2(create_dir2):
 
 
     path_with_file = shutil.copy(file, create_dir2/"folder1/", follow_symlinks=True)
+    path_with_file2 = shutil.copy(file, create_dir2/"folder1/folder2", follow_symlinks=True)
     print(path_with_file)
+    print(f'{path_with_file2=}')
     print(create_dir2.parent)
 
     print(get_names(create_dir2))
     print(type(create_dir2))
 
-    assert get_names(create_dir2) == [(path_with_file, str(create_dir2))]
+    assert get_names(create_dir2) == [(path_with_file, str(create_dir2/"folder1/"))]
 
     return
 
-
-
-
-##TODO
-# Quitar de get_names y visualizar_enpython que corran pathlib.Path - poner if my_path es ya un pathlib.Path, no lo corra
-# Por que no borra los dir temporales???
 
 #if __name__=="__main__":
 #    test_1(create_dir)
